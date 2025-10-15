2f78938b01c38e46f3674800b6204505876fce0e
echang14 <220988341+echang14@users.noreply.github.com>
2025-10-15 13:17:55 -0700

    install packages and compete setup rpoject shellscript

--- q1_setup_project.sh
+++ q1_setup_project.sh
@@ -1,4 +1,4 @@
-# TODO: Add shebang line: #!/bin/bash
+#!/bin/bash
 # Assignment 5, Question 1: Project Setup Script
 # This script creates the directory structure for the clinical trial analysis project
 # TODO: Make this script executable (if not already)
@@ -9,9 +9,23 @@
 #   - output/
 #   - reports/
 
+echo "Creat data, output, reports directories"
+mkdir -p data output reports
+echo "Directories created."
+
 # TODO: Generate the dataset
 #       Run: python3 generate_data.py
 #       This creates data/clinical_trial_raw.csv with 10,000 patients
 
+echo "Generate dataset"
+python3 generate_data.py
+echo "Dataset generated at data/clinical_trial_raw.csv"
+
 # TODO: Save the directory structure to reports/directory_structure.txt
 #       Hint: Use 'ls -la' or 'tree' command
+
+echo "Save directory structure to reports/directory_structure.txt"
+tree > reports/directory_structure.txt
+echo "Directory strusource datasci-practice/bin/activatecture saved to reports/directory_structure.txt"
+
+chmod +x q1_setup_project.sh
